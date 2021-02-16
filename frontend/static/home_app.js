d54b98068bf99dafdad7c870c8f67208d28f9064
Jakub <mail>
2021-02-16 19:26:01 +0100

    losowanie_cytatow

--- frontend/static/home_app.js
+++ frontend/static/home_app.js
@@ -1,5 +1,10 @@
 const select = document.querySelector('.custom-select')
 
+const tresc = document.querySelector('#cytat')
+const podpis = document.querySelector('#autor')
+
+const author_img = document.querySelector('.img-thumbnail')
+
 function add_option(autor){
     const option = document.createElement('option')
     option.value = autor.id
@@ -8,9 +13,43 @@ function add_option(autor){
     select.appendChild(option)
 
 }
-
-
+const autorzy = []
+// #autorzy
 fetch('http://127.0.0.1:8000/api/autor/',{headers: {'Authorization': 'Basic ' + btoa('jakub:toor')}}
 ).then(response => response.json()
-).then(data => data.forEach(element => add_option(element)))
+).then(data => data.forEach(element => {
+    add_option(element)
+    autorzy.push(element)
+}))
+
+// cytaty
+
+const cytaty = []
+fetch('http://127.0.0.1:8000/api/cytaty/',{headers: {'Authorization': 'Basic ' + btoa('jakub:toor')}}
+).then(response =>response.json()).then(data => data.forEach(e=> cytaty.push(e)))
+
+
+
+function main(){
+    const cytaty_autora = (select.value=="0")? cytaty : cytaty.filter(obj => obj.autor == select.value)
+    
+    const cytat = cytaty_autora[Math.floor(Math.random() * cytaty_autora.length)];
+
+    if(tresc.innerText == cytat.tresc){
+        main()
+    }
+    else{
+        tresc.innerText = cytat.tresc
+
+        const autor = autorzy.find(e => e.id == cytat.autor)
+        
+        author_img.src = autor.zdjecie
+        podpis.innerText = autor.imie
+
+    }
+
+    
+}
+
+document.querySelector('.button').addEventListener('click',main)
 
